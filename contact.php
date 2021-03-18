96e312f5e05112a28df4cb31132f969eb9d949da
ACHREKARURMILA <64479949+ACHREKARURMILA@users.noreply.github.com>
2021-03-18 15:25:16 +0530

    first upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+$message = $_REQUEST['message'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+
+if (empty ($message) || empty ($name) || empty ($email))
+{
+	echo "please fill up all the fields.";
+	
+}
+else 
+{
+    mail("u.achrekar27@gmail.com","painting project message",$message ,"From: $name <$email>");
+	echo "<script type='text/javascript'> 
+	alert('your message submitted successfully');
+	history.back();
+	</script>";	
+}
+
+?>
+
